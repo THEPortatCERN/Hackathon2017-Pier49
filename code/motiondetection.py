cd57335f99bbeaf9137eef09a0e0e80118daac53
pier49_raspi_devel <none@none.com>
2017-10-08 11:37:27 +0000

    seperate process for motiondetection; OHF code updated cause new API

---
+++ code/motiondetection.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from smartbin import iocontrol
+
+if __name__=='__main__':
+    io = iocontrol()
+    io.playonmotion()
--- code/smartbin.py
+++ code/smartbin.py
@@ -15,6 +15,8 @@ from google.cloud import vision
 from google.cloud.vision import types
 import automationhat
 import requests, json
+import subprocess
+import hashlib
 
 class iocontrol:
     def __init__(self):
@@ -37,6 +39,21 @@ class iocontrol:
 
     def detect_motion(self):
         return automationhat.input.one.read()
+
+    def playonmotion(self):
+        """
+        plays an mp3 on rising edge of motion detection only
+        """
+        prevm = 0
+        while True:
+            nowm = self.detect_motion()
+            if prevm==0 and nowm==1:
+                print('motion detected!')
+                musicdir = os.path.abspath(os.path.join(os.path.dirname(__file__),"../mp3/"))
+                mp3 = musicdir+'/example.mp3';
+                os.system('/usr/bin/omxplayer -o local '+mp3)
+                time.sleep(0.1)
+            prevm = nowm
         
 class googlevisionapi:
     def __init__(self):
@@ -68,37 +85,36 @@ def ohf_post(trans, payload):
     one happy family server POST requests
     see https://github.com/mrcage/ohf-community
     """
-    url = 'https://app-test.ohf-lesvos.org/api/recyclingPoints/'+trans
+    url = 'https://app-test.ohf-lesvos.org/api/v1/person/'+trans
     headers = {'Accept':'application/json', 'Content-Type': 'application/json', 'User-Agent': 'raspbi-bin'}
-    r=0
-    if trans=='register' or trans=='getToken' or trans=='addPoints':
+    r="invalid choice for function"
+    if trans=='register' or trans=='getToken' or trans=='addBalance':
         r = requests.post(url, data=json.dumps(payload), headers=headers)
     return r
 
 def ohf_test():
+    """
+    API at https://github.com/mrcage/ohf-community/blob/recycling/api.md
+    """
     f = open('api_key.txt', 'r')
     apikey = f.read()
+    f.close()
 
-    payload = {"name": "deadbeef", "family_name": "Muster","case_no": "123456789012345", "password": "abcdef123"}
+    caseno = 123456789012345
+    payload = {"name": "deadbeef", "family_name": "Muster","case_no": caseno, "password": "abcdef123"}
     r = ohf_post('register',payload);
     print(r.text)
-    payload = {"case_no": "123456789012345", "password": "abcdef123"}
+    payload = {"case_no": caseno, "password": "abcdef123"}
     r = ohf_post('getToken', payload);
     print(r.text)
-    payload = {"api_key": apikey, "nonce": r.json()['nonce'], "value":666}
-    r = ohf_post('addPoints', payload);
+    print(hashlib.sha256(str(r.json()['nonce']).encode('utf-8')).hexdigest())
+    payload = {"api_key": apikey, "case_no": caseno, "nonce_hash": hashlib.sha256(str(r.json()['nonce']).encode('utf-8')).hexdigest(), "value":6}
+    r = ohf_post('addBalance', payload);
     print(r.text)
     
 if __name__ == '__main__':
-    io = iocontrol();
-    prevm = 0
-    while True:
-        if prevm==0 and io.detect_motion()==1:
-            print('motion detected!')
-            musicdir = os.path.abspath(os.path.join(os.path.dirname(__file__),"../mp3/"))
-            mp3 = musicdir+'/example.mp3';
-            os.system('/usr/bin/omxplayer -o local '+mp3)
-        time.sleep(0.1)
+    print("starting motiondetection.py in background..")
+    subprocess.Popen(["./motiondetection.py"])
     
     gv = googlevisionapi()
     gvfilter = {'bottle' : 80.0, 'plastic' : 60.0}
@@ -128,6 +144,8 @@ if __name__ == '__main__':
         print('\n test passed')
     else:
         print('\n test failed')
+        while True:
+            time.sleep(1)
         sys.exit(0)
 
     # here in case test has passed
